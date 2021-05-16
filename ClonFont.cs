9a6e7b2dbae7c81c36cbf5cef9fcbd5e03f67ad8
YoshiOG <m.yoshiog1@gmail.com>
2021-05-15 20:54:50 -0400

    innit bruv (add source files for the first time)

---
+++ ClonFont.cs
@@ -0,0 +1,126 @@
+﻿using BepInEx;
+using BiendeoCHLib;
+using BiendeoCHLib.Patches;
+using BiendeoCHLib.Patches.Attributes;
+using BiendeoCHLib.Wrappers;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.GUI;
+
+namespace ClonFont
+{
+	[BepInPlugin("com.yoshiog.clonfont", "ClonFont", "1.0.0.0")]
+	[BepInDependency("com.biendeo.biendeochlib")]
+	public class ClonFont : BaseUnityPlugin
+	{
+		public static ClonFont Instance { get; private set; }
+		private string sceneName;
+
+		private TextMeshProUGUI currPhrase;
+		private TextMeshProUGUI nextPhrase;
+		private TextMeshProUGUI fadePhrase;
+		private TextMeshProUGUI soloObj;
+
+		public bool sceneChanged;
+
+		private Font customFont;
+		private string fontPath;
+		private FileInfo fontFile;
+
+		private string lyricFontName;
+		private string hudSoloFontName;
+		private TMP_FontAsset lyricsFontAss;
+		private TMP_FontAsset hudSoloFontAss;
+		
+		
+		public ClonFont()
+        {
+			Instance = this;
+		}
+
+		public TMP_FontAsset LoadFont(string ourFont)
+        {
+			fontPath = Path.Combine(Paths.BepInExRootPath, "plugins", "ClonFont", "Fonts", ourFont + ".ttf");
+			fontFile = new FileInfo(fontPath);
+			TMP_FontAsset customFontAsset;
+			if (fontFile.Exists && ourFont.ToLower() != "default")
+			{
+				customFont = new Font(fontPath);
+				customFontAsset = TMP_FontAsset.CreateFontAsset(customFont);
+				return customFontAsset;
+			}
+			else
+            {
+				return null;
+            }
+		}
+
+		public void ReloadFontConfig()
+        {
+			string fontConfigPath = Path.Combine(Paths.BepInExRootPath, "plugins", "ClonFont", "fonts.ini");
+			if (!File.Exists(fontConfigPath))
+            {
+				IniBruv.IniWriteValue("Fonts", "LyricsFont", "default");
+				IniBruv.IniWriteValue("Fonts", "HUDSoloFont", "NOT CURRENTLY WORKING");
+            }
+			lyricFontName = IniBruv.IniReadValue("Fonts", "LyricsFont");
+			hudSoloFontName = IniBruv.IniReadValue("Fonts", "HUDSoloFont");
+			
+			lyricsFontAss = LoadFont(lyricFontName);
+			hudSoloFontAss = LoadFont(hudSoloFontName);
+			// TODO: implement menu font and others?
+		}
+
+		#region Unity Methods
+		public void Start()
+		{
+			
+			SceneManager.activeSceneChanged += delegate (Scene _, Scene __)
+			{
+				sceneChanged = true;
+			};
+			ReloadFontConfig();
+		}
+
+		public void LateUpdate()
+		{
+			sceneName = SceneManager.GetActiveScene().name;
+			if (sceneName == "Gameplay")
+			{
+				if (lyricsFontAss != null)
+				{
+					currPhrase = GameObject.Find("CurrentPhrase").GetComponent<TextMeshProUGUI>();
+					nextPhrase = GameObject.Find("NextPhrase").GetComponent<TextMeshProUGUI>();
+					fadePhrase = GameObject.Find("fadePhrase").GetComponent<TextMeshProUGUI>();
+					currPhrase.font = lyricsFontAss;
+					nextPhrase.font = lyricsFontAss;
+					fadePhrase.font = lyricsFontAss;
+				}
+				if (hudSoloFontAss != null)
+                {
+					// Doesn't seem to work atm, I'll have to figure out the solo HUD's object name
+					soloObj = GameObject.Find("Solo Counter").GetComponent<TextMeshProUGUI>();
+					soloObj.font = hudSoloFontAss;
+                }
+			}
+			if (this.sceneChanged)
+            {
+				ReloadFontConfig();
+				this.sceneChanged = false;
+			}
+		}
+		#endregion
+	}
+}
---
+++ IniBruv.cs
@@ -0,0 +1,129 @@
+﻿using BepInEx;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClonFont
+{
+    // I bascially took this code from Unity Forums lol sorry
+    public class IniBruv
+    {
+        private static string path = Path.Combine(Paths.BepInExRootPath, "plugins", "ClonFont", "fonts.ini");
+        private static Dictionary<string, Dictionary<string, string>> IniDictionary = new Dictionary<string, Dictionary<string, string>>();
+        private static bool Initialized = false;
+
+        public enum Sections
+        {
+            Section01,
+        }
+
+        public enum Keys
+        {
+            Key01,
+            Key02,
+            Key03,
+        }
+
+        private static bool FirstRead()
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    string theSection = "";
+                    string theKey = "";
+                    string theValue = "";
+                    while (!string.IsNullOrEmpty(line = sr.ReadLine()))
+                    {
+                        line.Trim();
+                        if (line.StartsWith("[") && line.EndsWith("]"))
+                        {
+                            theSection = line.Substring(1, line.Length - 2);
+                        }
+                        else
+                        {
+                            string[] ln = line.Split(new char[] { '=' });
+                            theKey = ln[0].Trim();
+                            theValue = ln[1].Trim();
+                        }
+                        if (theSection == "" || theKey == "" || theValue == "")
+                            continue;
+                        PopulateIni(theSection, theKey, theValue);
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static void PopulateIni(string _Section, string _Key, string _Value)
+        {
+            if (IniDictionary.ContainsKey(_Section))
+            {
+                if (IniDictionary[_Section].ContainsKey(_Key))
+                    IniDictionary[_Section][_Key] = _Value;
+                else
+                    IniDictionary[_Section].Add(_Key, _Value);
+            }
+            else
+            {
+                Dictionary<string, string> neuVal = new Dictionary<string, string>();
+                neuVal.Add(_Key.ToString(), _Value);
+                IniDictionary.Add(_Section.ToString(), neuVal);
+            }
+        }
+
+        public static void IniWriteValue(string _Section, string _Key, string _Value)
+        {
+            if (!Initialized)
+                FirstRead();
+            PopulateIni(_Section, _Key, _Value);
+            //write ini
+            WriteIni();
+        }
+
+        public static void IniWriteValue(Sections _Section, Keys _Key, string _Value)
+        {
+            IniWriteValue(_Section.ToString(), _Key.ToString(), _Value);
+        }
+
+        private static void WriteIni()
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (KeyValuePair<string, Dictionary<string, string>> sezioni in IniDictionary)
+                {
+                    sw.WriteLine("[" + sezioni.Key.ToString() + "]");
+                    foreach (KeyValuePair<string, string> chiave in sezioni.Value)
+                    {
+                        // value must be in one line
+                        string vale = chiave.Value.ToString();
+                        vale = vale.Replace(Environment.NewLine, " ");
+                        vale = vale.Replace("\r\n", " ");
+                        sw.WriteLine(chiave.Key.ToString() + " = " + vale);
+                    }
+                }
+            }
+        }
+
+        public static string IniReadValue(Sections _Section, Keys _Key)
+        {
+            if (!Initialized)
+                FirstRead();
+            return IniReadValue(_Section.ToString(), _Key.ToString());
+        }
+        public static string IniReadValue(string _Section, string _Key)
+        {
+            if (!Initialized)
+                FirstRead();
+            if (IniDictionary.ContainsKey(_Section))
+                if (IniDictionary[_Section].ContainsKey(_Key))
+                    return IniDictionary[_Section][_Key];
+            return null;
+        }
+    }
+}
